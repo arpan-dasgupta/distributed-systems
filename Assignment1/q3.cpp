c2177aef83bda2711d68ac034d26822e19a75281
Arpan Dasgupta <arpandasgupta0@gmail.com>
2021-01-21 01:31:38 +0530

    Working on q3

--- Assignment1/q3.cpp
+++ Assignment1/q3.cpp
@@ -11,48 +11,14 @@ int numpr;
 #define send_data_tag 2001
 #define return_data_tag 2002
 
-int partition(vector<int> &arr, int l, int r)
-{
-    int pvt = l + rand() % (r - l + 1);
-    // for (ll i = l; i <= r; i++)
-    // {
-    //     cout << arr[i] << " ";
-    // }
-    // cout << '\n';
-    swap(arr[pvt], arr[r]);
-    int fe = l;
-    for (int i = l; i < r; i++)
-    {
-        if (arr[i] < arr[r])
-        {
-            swap(arr[i], arr[fe]);
-            fe++;
-        }
-    }
-    // for (ll i = l; i <= r; i++)
-    // {
-    //     cout << arr[i] << " ";
-    // }
-    // cout << '\n';
-    swap(arr[r], arr[fe]);
-    // for (ll i = l; i <= r; i++)
-    // {
-    //     cout << arr[i] << " ";
-    // }
-    // cout << '\n';
-    return fe;
-}
+int nump;
 
-void quicksort(vector<int> &arr, int low, int high)
+typedef struct graph_data
 {
-    // cout << low << " " << high << '\n';
-    if (low < high)
-    {
-        int pos = partition(arr, low, high);
-        quicksort(arr, low, pos - 1);
-        quicksort(arr, pos + 1, high);
-    }
-}
+    vector<vector<int>> adj;
+    vector<int> colors;
+    int l, r;
+} gd;
 
 void send_vec(vector<int> &arr, int rank)
 {
@@ -70,26 +36,69 @@ void receive_vec(vector<int> &arr, int rank)
     MPI_Recv(&arr[0], num, MPI_INT, rank, send_data_tag, MPI_COMM_WORLD, &stat);
 }
 
-void merge(vector<vector<int>> &all, vector<int> &arr)
+void color_reduce(vector<int> &color, int l, int r, vector<vector<int>> &adj, int del)
 {
-    int num = all.size();
-    multiset<pair<int, int>> mst;
-    vector<int> indices(num, 0);
-    for (int i = 0; i < num; i++)
+    for (int i = del + l + 1; i <= r; i++)
     {
-        mst.insert({all[i][0], i});
-        indices[i]++;
-    }
-    while (mst.size() > 0)
-    {
-        auto a = *mst.begin();
-        arr.push_back(a.first);
-        mst.erase(mst.begin());
-        if (indices[a.second] < all[a.second].size())
+        int mx = l;
+        set<int> used;
+        for (auto vert : adj[i])
         {
-            mst.insert({all[a.second][indices[a.second]], a.second});
-            indices[a.second]++;
+            used.insert(color[vert]);
         }
+        while (used.find(mx) != used.end())
+        {
+            mx++;
+        }
+        color[i] = mx;
+    }
+}
+
+void combine_reduce(vector<int> &color, int l, int r, int mid, vector<vector<int>> &adj, int del)
+{
+    for (int i = mid + 1; i <= r; i++)
+    {
+        int mx = l;
+        set<int> used;
+        for (auto vert : adj[i])
+        {
+            used.insert(color[vert]);
+        }
+        while (used.find(mx) != used.end())
+        {
+            mx++;
+        }
+        color[i] = mx;
+    }
+    // for (int i = mid; i <= r; i++)
+    // {
+    //     int mx = l;
+    //     set<int> used;
+    //     for (auto vert : adj[i])
+    //     {
+    //         used.insert(color[vert]);
+    //     }
+    //     while (used.find(mx) != used.end())
+    //     {
+    //         mx++;
+    //     }
+    //     color[i] = mx;
+    // }
+}
+
+void color_reduce_parallel(vector<int> &color, int l, int r, vector<vector<int>> &adj, int depth, int rank, int del)
+{
+    int mid = (l + r) / 2;
+    int next_rank = pow(rank + 2, depth);
+    if (next_rank < numpr)
+    {
+        color_reduce_parallel(color, l, mid, adj, depth + 1, rank, del);
+
+        combine_reduce(color, l, r, mid, adj, del);
+    }
+    else
+    {
+        color_reduce(color, l, r, adj, del);
     }
 }
 
@@ -97,7 +106,7 @@ int main(int argc, char **argv)
 {
 
     freopen(argv[1], "r", stdin);
-    freopen(argv[2], "w", stdout);
+    freopen(argv[2], "a", stdout);
 
     int rank, numprocs;
 
@@ -112,94 +121,57 @@ int main(int argc, char **argv)
     double tbeg = MPI_Wtime();
     // vector<pair<int,int>> trans
 
-    int n;
+    int nump = numprocs;
+
+    int n, del = 0;
     vector<int> arr;
     if (rank == 0)
     {
-        cin >> n;
-        arr.resize(n);
-        for (int i = 0; i < n; i++)
+        int n, m;
+        cin >> n >> m;
+        vector<pair<int, int>> edges;
+        for (int i = 0; i < m; i++)
         {
-            cin >> arr[i];
+            int u, v;
+            cin >> u >> v;
+            edges.push_back({u, v});
         }
-        // quicksort(arr, 0, n - 1);
-        // for (ll i = 0; i < n; i++)
-        // {
-        //     cout << arr[i] << " ";
-        // }
-        vector<int> start_points(numprocs);
-        int jumps = (n + numprocs - 1) / numprocs;
-        for (int i = 0; i < numprocs; i++)
+        vector<vector<int>> adj(m);
+        for (int i = 0; i < m; i++)
         {
-            start_points[i] = i * jumps;
+            for (int j = i + 1; j < m; j++)
+            {
+                if ((edges[i].first == edges[j].first || edges[i].second == edges[j].first || edges[i].first == edges[j].second || edges[i].second == edges[j].second))
+                {
+                    adj[i].push_back(j);
+                    adj[j].push_back(i);
+                }
+            }
         }
-        vector<vector<int>> all_vecs;
-        vector<int> my_vec;
-        for (int i = 0; i < numprocs; i++)
+        for (int i = 0; i < m; i++)
         {
-            vector<int> nv;
-            for (int j = start_points[i]; j < min(start_points[i] + jumps, n); j++)
-            {
-                nv.push_back(arr[j]);
-            }
-            if (i == 0)
-            {
-                // quicksort(nv, 0, int(nv.size()) - 1);
-                my_vec = vector<int>(nv);
-                continue;
-            }
-            int val = nv.size();
-            // for (int i = 0; i < val; i++)
+            // for (auto neigh : adj[i])
             // {
-            //     cout << nv[i] << " ";
+            //     cout << neigh << " ";
             // }
-            // cout << " ++ \n";
-            send_vec(nv, i);
-            // vector<int> sv;
-            // receive_vec(sv, i);
+            // cout << '\n';
+            del = max(del, int(adj[i].size()));
         }
-        quicksort(my_vec, 0, int(my_vec.size()) - 1);
-        for (int i = 0; i < numprocs; i++)
-        {
-            if (i == 0)
-                all_vecs.push_back(my_vec);
-            else
-            {
-                vector<int> sv;
-                receive_vec(sv, i);
-                if (sv.size() > 0)
-                    all_vecs.push_back(sv);
-            }
-        }
-        // for (int i = 0; i < all_vecs.size(); i++)
+        MPI_Bcast(&del, 1, MPI_INT, 0, MPI_COMM_WORLD);
+        vector<int> color(m, 0);
+        for (int i = 0; i < m; i++)
+            color[i] = i;
+        // color_reduce(color, 0, m - 1, adj, del);
+        // cout << "del = " << del << '\n';
+        // for (int i = 0; i < m; i++)
         // {
-        //     for (int j = 0; j < all_vecs[i].size(); j++)
-        //     {
-        //         cout << all_vecs[i][j] << " ";
-        //     }
-        //     cout << '\n';
+        //     cout << color[i] << " ";
         // }
-        vector<int> final;
-        merge(all_vecs, final);
-        for (int i = 0; i < final.size(); i++)
-        {
-            cout << final[i] << "\n";
-        }
+        // cout << '\n';
+        color_reduce_parallel(color, 0, m - 1, adj, 0, 0, del);
     }
     else
     {
-        vector<int> arr;
-        // MPI_Send(&nv, nv.size(), MPI_INT, i, 0, MPI_COMM_WORLD);
-        // cout << num << '\n';
-        receive_vec(arr, 0);
-        int num = arr.size();
-        quicksort(arr, 0, num - 1);
-        // for (int i = 0; i < num; i++)
-        // {
-        //     cout << arr[i] << " ";
-        // }
-        // cout << '\n';
-        send_vec(arr, 0);
     }
 
     // MPI_Bcast(&n, 1, MPI_INT, 0, MPI_COMM_WORLD);
@@ -210,7 +182,7 @@ int main(int argc, char **argv)
     MPI_Reduce(&elapsedTime, &maxTime, 1, MPI_DOUBLE, MPI_MAX, 0, MPI_COMM_WORLD);
     if (rank == 0)
     {
-        // printf("Total time (s): %f\n", maxTime);
+        printf("Total time (s): %f\n", maxTime);
     }
 
     /* shut down MPI */
