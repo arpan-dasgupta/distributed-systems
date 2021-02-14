ba15e6c938ac04440f48a56c7902d01c810cb582
Arpan Dasgupta <arpandasgupta0@gmail.com>
2021-02-15 00:19:05 +0530

    Finished q1

--- Assignment2/2018111010_1.erl
+++ Assignment2/2018111010_1.erl
@@ -1,6 +1,6 @@
 -module('2018111010_1').
 % -module(multi_rpc_test).
--export([main/1, ring_node/1]). 
+-export([main/1, rnode/2]). 
 
 read_two_ints(InpDevice) ->
     case io:fread(InpDevice,"kjds", "~d ~d") of
@@ -12,37 +12,60 @@ read_two_ints(InpDevice) ->
 %     [NodeCount,Token] = read_two_ints(Input),
 %     io:fwrite("~w",[NodeCount]),
 %     io:fwrite("~w",[Token]),
-loop(0) ->
-    ok;
-loop(X) ->
-    io:format("~p processing ~p\n", [self(), X]),
-    loop(X-1).
+% loop(0) ->
+%     ok;
+% loop(X) ->
+%     io:format("~p processing ~p\n", [self(), X]),
+%     loop(X-1).
 
-
-% node(NodeNo, Token, NodeCount) ->
-%     ;
-% node(NodeNo, NodeCount) ->
-%     .
-
-ring_node(I) ->
-    io:format("process created: ~w~n", [I] ).
-
-for (N, N) ->
-    [spawn('2018111010_1',ring_node,[N])];   
-for (I, N) ->
-    [spawn('2018111010_1',ring_node,[I])| for(I+1, N)].
+rnode(0, OutputFile) ->
+    % io:format("process created: ~w~n", [999] ),
+    receive
+        {Token,  [H | T]} -> 
+            % io:format("Process started, process 0 has token ~p~n",[Token]),
+            % io:format("Process started, process 0 has token ~p~n",[H]),
+            % io:format("Process started, process 0 has token ~p~n",[T]),
+            H ! {0, Token,  T},
+            rnode(0, OutputFile);
+        {From, Token,  []} -> 
+            {ok, Output} = file:open(OutputFile, [append]),
+            io:format(Output, "Process 0 received token ~w from process ~w.~n",[Token, From])
+            % io:format( "Process 0 received token ~w from process ~w~n",[Token, From])
+    end;
+rnode(NodeNo, OutputFile) ->
+    % io:format("process created: ~w~n", [NodeNo] ),
+    receive
+        {From, Token,  [H|T]} ->
+            {ok, Output} = file:open(OutputFile, [append]),
+            io:format(Output, "Process ~w received token ~w from process ~w.~n",[NodeNo, Token, From]),
+            % io:format( "Process ~w has received token ~w from process ~w~n",[NodeNo, Token, From]),
+            % io:format("Process started, process 0 has token ~p~n",[T]),
+            H ! {NodeNo, Token,  T}
+    end.
 
 main(Args) -> 
     [InputFile, OutputFile] = Args,
     {ok, Input} = file:open(InputFile, [read]),
     {ok, Output} = file:open(OutputFile, [write]),
-    [NodeCount,Token] = read_two_ints(Input),
+    [NodeCount, Token] = read_two_ints(Input),
     % io:fwrite("~w",[NodeCount]),
     % io:fwrite("~w",[Token]),
 
     % spawn('2018111010_1',node,[0,Token]),
-    Processes = [spawn(fun() -> loop(1) end) || _ <- lists:seq(1, NodeCount)],
-    [H | _] = Processes,
+    Processes = [spawn(fun() -> rnode(I,OutputFile) end) || I <- lists:seq(0, NodeCount-1)],
+    % [H | _] = Processes,
+    io:fwrite(Output,"",[]),
+    % io:fwrite(Output,"",[]),
+
+    [Start | T ] = Processes,
+    % io:fwrite("~w",[Start]),
+    % AllH = [ T | Start],
+    AllH = lists:append(T,[Start]),
+    % io:format("~p~n", [AllH]),
+    
+    Start ! {Token, AllH},
     % L = for(1, NodeCount-1),
-    % io:format("~p~n", [L]),
-    file:close(Input).
+    % io:format("~p~n", [H]),
+    file:close(Input),
+    file:close(Input)
+    .
